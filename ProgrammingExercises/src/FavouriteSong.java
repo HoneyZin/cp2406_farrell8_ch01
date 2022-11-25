51c12a266e8d619b569717cb0a9edcf5133d592b
Honey Zin <honey27zin@gmail.com>
2022-11-25 15:27:45 +0800

    Add programming exercises

---
+++ ProgrammingExercises/src/FavouriteSong.java
@@ -0,0 +1,10 @@
+class FavoriteSong  //favourite lyrics from my fav song A Million Dreams
+{
+    public static void main(String[] args)
+    {
+        System.out.println("They can say, they can say it all sounds crazy");
+        System.out.println("They can say, they can say I've lost my mind");
+        System.out.println("I don't care, I don't care, so call me crazy");
+        System.out.println("We can live in a world that we design");
+    }
+}
---
+++ ProgrammingExercises/src/MovieQuote.java
@@ -0,0 +1,7 @@
+class MovieQuote
+{
+    public static void main(String[] args)
+    {
+        System.out.println("I feel like I'm dreaming and I'm about to wake up."); //Fav movie quote
+    }
+}
